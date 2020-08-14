741b063c15c6dd0fd695869f23de552de50ed36e
Brian Harvey <brian.harv3y@gmail.com>
2020-08-14 10:29:14 -0700

    define variables in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,21 @@
 
 //User Interface Logic
 $(document).ready(function() {
-  $("#form1").submit();
-  $("#form2").submit();
-  $("#form3").submit();
-  $("#form4").submit();
-  $("#form5").submit();
+  $("#form1").submit(function() {
+  $("#form2").submit(function() {
+  $("#form3").submit(function() {
+  $("#form4").submit(function() {
+  $("#form5").submit(function() {
   event.preventDefault();
+  const number1 = parseInt($("#favfood").val());
+  const number2 = parseInt($("#siblings").val());
+  const number3 = parseInt($("#favmovie").val());
+  const number4 = parseInt($("#pet").val());
+  const number5 = parseInt($("#car").val());
 
+          });
+        });
+      });
+    });
+  });
 });

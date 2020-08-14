ca3f0b2a8b34e3ddf3db30e2f1f7781c28c381f2
Brian Harvey <brian.harv3y@gmail.com>
2020-08-14 15:12:09 -0700

    prevent result from showing before all boxes selected

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,10 @@ const add =function(number1, number2, number3, number4, number5) {
   return number1 + number2 + number3 + number4 + number5;
 }
 
-//let form1Changed = false;
-//let form2Changed = false;
-//let form3Changed = false;
-//let form4Changed = false;
-//let form5Changed = false;
 
 //User Interface Logic
 $(document).ready(function() {
-  $("#form1").change(function() {
-    console.log("working1");
-  $("#form2").change(function() {
-    console.log("working2");
-  $("#form3").change(function() {
-    console.log("working3");
-  $("#form4").change(function() {
-    console.log("working4");
-  $("#form5").change(function() {
-    console.log("working5");
+  $("#form1, #form2, #form3, #form4, #form5").change(function() {
   event.preventDefault();
   const number1 = parseInt($("#favfood").val());
   const number2 = parseInt($("#siblings").val());
@@ -28,21 +14,19 @@ $(document).ready(function() {
   const number4 = parseInt($("#pet").val());
   const number5 = parseInt($("#car").val());
   let result = add(number1, number2, number3, number4, number5);
-    console.log("add working");
-    if (result > 26) {
-      $("#output").text("Ruby");
-    } else if (result < 26 && result > 18) {
-      $("#output").text("C#");
-    } else if (result < 18 && result >= 10) {
-      $("#output").text(".NET");
+
+    if (result >= 25 && number1 > 1 && number2 > 1 && number3 > 1 && number4 > 1 && number5 > 1) {
+      $("#output-ruby").text("Ruby");
+      $("#form-output-ruby").show();
+    } else if (result <= 24 && result >= 17 && number1 > 1 && number2 > 1 && number3 > 1 && number4 > 1 && number5 > 1) {
+      $("#output-javascript").text("Javascript");
+      $("#form-output-javascript").show();
+    } else if (result <= 16 && result >= 10 && number1 > 1 && number2 > 1 && number3 > 1 && number4 > 1 && number5 > 1) {
+      $("#output-python").text("Python");
+      $("#form-output-python").show();
+    } else {
+      $("#form-output-ruby, #form-output-javascript, #form-output-python").hide();
     }
-
-  $("#form-output").show();
-
-          });
-        });
-      });
-    });
   });
 }); 
 /*
@@ -96,6 +80,12 @@ $(document).ready(function() {
       alert("working");
     }
   });
-});    
+});   
+
+ $("#form1").change(function() {
+  $("#form2").change(function() {
+  $("#form3").change(function() {
+  $("#form4").change(function() {
+  $("#form5").change(function() {
 
 */

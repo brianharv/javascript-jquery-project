d5adccb2eb62f8ea6eeb0dadb07e70776a371599
Brian Harvey <brian.harv3y@gmail.com>
2020-08-14 09:53:33 -0700

    add early jquery functions to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//Business Logic
+
+
+//User Interface Logic
+$(document).ready(function() {
+  $("#form1").submit();
+  $("#form2").submit();
+  $("#form3").submit();
+  $("#form4").submit();
+  $("#form5").submit();
+  event.preventDefault();
+
+});

a7cdb9131cb5f01005c726c0611fad8b8833daea
Brian Harvey <brian.harv3y@gmail.com>
2020-08-14 11:59:43 -0700

    troubleshoot change method and scripts logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,17 @@
 
 
 //User Interface Logic
-$(document).ready(function() {
-  $("#form1").submit(function() {
-  $("#form2").submit(function() {
-  $("#form3").submit(function() {
-  $("#form4").submit(function() {
-  $("#form5").submit(function() {
+/*$(document).ready(function() {
+  $("#form1").change(function() {
+    alert("working1");
+  $("#form2").change(function() {
+    alert("working2");
+  $("#form3").change(function() {
+    alert("working3");
+  $("#form4").change(function() {
+    alert("working4");
+  $("#form5").change(function() {
+    alert("working5");
   event.preventDefault();
   const number1 = parseInt($("#favfood").val());
   const number2 = parseInt($("#siblings").val());
@@ -20,4 +25,46 @@ $(document).ready(function() {
       });
     });
   });
-});
+}); */
+
+$(document).ready(function() {
+  $("#form1").on('change',function() {
+    let form1Changed = false;
+    form1Changed = true;
+    if (form2Changed && form3Changed && form4Changed && form5Changed) {
+      alert("working");
+    }
+  });
+
+  $("#form2").on('change',function() {
+    let form2Changed = false;
+    form2Changed = true;
+    if (form1Changed && form3Changed && form4Changed && form5Changed) {
+      alert("working");
+    }
+  });
+
+  $("#form3").on('change',function() {
+    let form3Changed = false;
+    form3Changed = true;
+    if (form1Changed && form2Changed && form4Changed && form5Changed) {
+      alert("working");
+    }
+  });
+  
+  $("#form4").on('change',function() {
+    let form4Changed = false;
+    form4Changed = true;
+    if (form1Changed && form2Changed && form3Changed && form5Changed) {
+      alert("working");
+    }
+  });
+  
+  $("#form5").on('change',function() {
+    let form5Changed = false;
+    form5Changed = true;
+    if (form1Changed && form2Changed && form3Changed && form4Changed) {
+      alert("working");
+    }
+  });
+});    
